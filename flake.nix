6ac0dd1b576ce784ff8ee87c83a2b08006372601
trev <me@trev.xyz>
2025-08-27 11:45:14 -0400

    bump: v1.4.5 -> v1.4.6

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
 
       packages.default = pkgs.buildNpmPackage (finalAttrs: {
         pname = "nix-simple-cache-action";
-        version = "1.4.5";
+        version = "1.4.6";
         src = ./.;
         nodejs = pkgs.nodejs_20;
 
